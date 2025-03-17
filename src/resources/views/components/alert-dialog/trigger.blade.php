6231688832b408d05929ac98ebdedce802ce92cf
Zai185 <mkhant185@gmail.com>
2025-03-17 18:00:29 +0700

    update alert dialog & dropdown

--- src/resources/views/components/alert-dialog/trigger.blade.php
+++ src/resources/views/components/alert-dialog/trigger.blade.php
@@ -1,6 +1,3 @@
-<button
-    @click="open = true"
-    class="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-700 transition"
->
+<div @click="open = true">
     {{ $slot }}
-</button>
+</div>
