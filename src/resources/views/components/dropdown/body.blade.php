e73e8edfb961d95e4eac576c6d5047b139dcde1e
Zai185 <mkhant185@gmail.com>
2025-03-18 10:52:48 +0700

    fix: dropdown component

---
+++ src/resources/views/components/dropdown/body.blade.php
@@ -0,0 +1,3 @@
+<x-slot name="body">
+    {{ $slot }}
+</x-slot>
--- src/resources/views/components/dropdown/trigger.blade.php
+++ src/resources/views/components/dropdown/trigger.blade.php
@@ -1,27 +1,7 @@
-@props([
-    'variant' => 'primary',
-    'size' => 'sm',
-])
-
-@php
-
-    $base =
-        'mb-0.5 inline-flex h-10 items-center justify-center gap-1 rounded-md bg-default px-3.5 text-sm text-default-text transition-colors duration-200 ease-in-out hover:opacity-90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-main active:brightness-90 disabled:pointer-events-none disabled:opacity-50';
-
-    $variantClass = [
-        'primary' => 'hover:bg-primary',
-    ][$variant];
-    $sizeClass = [
-        'sm' => 'max-w-sm',
-        'md' => 'max-w-md',
-        'lg' => 'max-w-lg',
-    ][$size];
-
-@endphp
-
-
 <x-slot name="trigger">
-    <div x-on:click="dropdownOpen = !dropdownOpen" x-ref="trigger" >
+    <div {{ $attributes->merge([
+        'x-on:click' => 'dropdownOpen = !dropdownOpen;',
+    ]) }} x-ref="trigger">
         {{ $slot }}
     </div>
 </x-slot>
