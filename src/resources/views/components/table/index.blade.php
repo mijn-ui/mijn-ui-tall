510813bf7db58476098d5c006820707c7a640318
Zai185 <mkhant185@gmail.com>
2025-04-29 20:52:00 +0700

    fix table attribute merge

--- src/resources/views/components/table/index.blade.php
+++ src/resources/views/components/table/index.blade.php
@@ -1,18 +1,16 @@
 @props([
     'paginate' => null,
-    'perPage' => null
+    'perPage' => null,
 ])
 
 <div>
-    <div {{$attributes->merge([
-        'class' => 'relative w-full overflow-auto rounded-2xl border border-main-border bg-surface'
-    ])}}>
-        <table class="w-full text-left text-sm text-main-text rtl:text-right">
+    <div class = 'relative w-full overflow-auto rounded-2xl border border-main-border bg-surface'>
+        <table {{ ['class' => 'w-full text-left text-sm text-main-text rtl:text-right'] }}>
             {{ $slot }}
         </table>
     </div>
 
     <?php if($paginate) :?>
-        <mijnui:pagination :data="$paginate" :$perPage />
+    <mijnui:pagination :data="$paginate" :$perPage />
     <?php endif;?>
 </div>
