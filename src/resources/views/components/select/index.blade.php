060120da300ce322a8d240d71f4a2bfff24c4a58
Zai185 <mkhant185@gmail.com>
2025-06-06 17:37:43 +0700

    add select component

--- src/resources/views/components/select/index.blade.php
+++ src/resources/views/components/select/index.blade.php
@@ -3,20 +3,26 @@
     'placeholder' => 'Select an option',
     'label' => null,
     'description' => null,
-    'size' => 'sm',
-    'variant' => 'primary',
+    'size' => 'default',
+    'variant' => 'surface',
+    'native' => false,
+    'multiple' => false,
 ])
 
 @php
-    $base = 'relative px-4 py-2 bg-white border rounded cursor-pointer select-none ';
+    $base = 'relative px-4 py-2 border rounded cursor-pointer select-none ';
     $variantClass = [
         'primary' => 'bg-primary',
+        'surface' => 'bg-surface',
     ][$variant];
     $sizeClass = [
+        'default' => 'w-full',
         'sm' => 'max-w-sm',
         'md' => 'max-w-md',
         'lg' => 'max-w-lg',
     ][$size];
+
+    $class = "$base $variantClass $sizeClass";
 
     $message ??= $name ? $errors->first($name) : null;
 
@@ -25,36 +31,51 @@
 @endphp
 
 <mijnui:with-field :$label :$description :$name>
-    <div x-data="{
-        value: @if ($attributes->wire('model')) @entangle($attributes->wire('model'))
-        @else 
-            null @endif
-    }">
-        <input type="hidden" {{ $attributes->except('class') }}
-            @isset($name) name="{{ $name }}" @endisset />
-
-        <!-- ComboBox -->
-        <div class="flex flex-col justify-center gap-1 relative" x-data="{
+    @if ($native)
+        <select x-data="{
             selectOpen: false,
             selectedItem: '{{ $placeholder }}',
             selectedValue: '',
             chosenText: {}
-        }"
-            x-on:click.outside="selectOpen = false">
-            <!-- ComboBox Trigger -->
-            <button x-on:click="selectOpen = !selectOpen" type="button" role="combobox" x-ref="selectTrigger"
-                class="flex h-10 items-center justify-between rounded-md border bg-surface px-3 py-2 text-sm placeholder:text-muted-text focus:border-ring focus:outline-none focus:ring-1 focus:ring-ring">
-                <span class="line-clamp-1" x-text="selectedItem">{{ $placeholder }}</span>
-                <svg stroke="currentColor" fill="none" viewBox="0 0 24 24" height="1em" width="1em"
-                    xmlns="http://www.w3.org/2000/svg" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="m7 15 5 5 5-5" />
-                    <path d="m7 9 5-5 5 5" />
-                </svg>
-            </button>
+        }" @isset($name) name="{{ $name }}" @endisset
+            {{ $attributes->merge([
+                'class' => $class,
+            ]) }}>
+            {{ $slot }}
+        </select>
+    @else
+        <div x-data="{
+            value: @if ($attributes->wire('model')) @entangle($attributes->wire('model'))
+        @else 
+            @if ($multiple) [] @else null @endif
+            @endif
+        }">
+            <input type="hidden" {{ $attributes->except('class') }}
+                @isset($name) name="{{ $name }}" @endisset x-bind:value="value" />
 
-            <!-- ComboBox Content -->
-            <div x-cloak x-show="selectOpen"
-                x-effect="
+            <!-- ComboBox -->
+            <div class="flex flex-col justify-center gap-1 relative" x-data="{
+                selectOpen: false,
+                selectedItem: value ?? '{{ $placeholder }}',
+                selectedValue: @if ($multiple) [] @else '' @endif,
+                chosenText: {}
+            }"
+                x-on:click.outside="selectOpen = false">
+                <!-- ComboBox Trigger -->
+                <button x-on:click="selectOpen = !selectOpen" type="button" role="combobox" x-ref="selectTrigger"
+                    class="flex h-10 items-center justify-between rounded-md border px-3 py-2 text-sm placeholder:text-muted-text focus:border-ring focus:outline-none focus:ring-1 focus:ring-ring {{ $variantClass }} {{ $sizeClass }}">
+                    <span class="line-clamp-1" x-text="selectedItem">{{ $placeholder }}</span>
+                    <svg stroke="currentColor" fill="none" viewBox="0 0 24 24" height="1em" width="1em"
+                        xmlns="http://www.w3.org/2000/svg" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round">
+                        <path d="m7 15 5 5 5-5" />
+                        <path d="m7 9 5-5 5 5" />
+                    </svg>
+                </button>
+
+                <!-- ComboBox Content -->
+                <div x-cloak x-show="selectOpen"
+                    x-effect="
             if (selectOpen) {
                 $nextTick(() => {
                     if ($refs.selectTrigger.getBoundingClientRect().bottom + $el.scrollHeight > window.innerHeight && $refs.selectTrigger.getBoundingClientRect().top > window.innerHeight /2) {
@@ -71,7 +92,7 @@
                 });
             }
     "
-                x-on:scroll.window="
+                    x-on:scroll.window="
             $nextTick(() => {
                 if ($refs.selectTrigger.getBoundingClientRect().bottom + $el.scrollHeight > window.innerHeight && $refs.selectTrigger.getBoundingClientRect().top > window.innerHeight /2) {
                     if (!$el.classList.contains('bottom-full')) {
@@ -86,19 +107,19 @@
                 }
             })
         "
-                x-transition
-                class="absolute w-full left-0 rounded-lg border bg-surface p-1 text-sm text-main-text z-50 overflow-hidden mt-1 max-h-[50vh] overflow-y-auto">
+                    x-transition
+                    class="absolute w-full left-0 rounded-lg border bg-surface p-1 text-sm text-main-text z-50 overflow-hidden mt-1 max-h-[50vh] overflow-y-auto ">
 
-                {{ $slot }}
+                    {{ $slot }}
+
+
+                </div>
+
 
 
             </div>
 
 
-
         </div>
-
-
-    </div>
-
+    @endif
 </mijnui:with-field>
