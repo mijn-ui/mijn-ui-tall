4a99350e69ad2742d159399e6a07ea21b5b877c1
Zai185 <mkhant185@gmail.com>
2025-06-06 10:47:16 +0700

    update select component

--- src/resources/views/components/select/index.blade.php
+++ src/resources/views/components/select/index.blade.php
@@ -20,63 +20,41 @@
 
     $message ??= $name ? $errors->first($name) : null;
 
-    $classes = "text-xs text-danger " . ($message ? '' : 'hidden');
+    $classes = 'text-xs text-danger ' . ($message ? '' : 'hidden');
 
 @endphp
 
 <mijnui:with-field :$label :$description :$name>
-<div x-data="{value : ''}" x-init="$watch('value', (value) => {
-            $wire.set('{{ $name }}', value);
-    })">
-    <input
-        x-model="value"
-        type="hidden"
-        {{ $attributes->except('class') }}
-        @isset($name) name="{{ $name }}" @endisset
-    />
+    <div x-data="{
+        value: @if ($attributes->wire('model')) @entangle($attributes->wire('model'))
+        @else 
+            null @endif
+    }">
+        <input type="hidden" {{ $attributes->except('class') }}
+            @isset($name) name="{{ $name }}" @endisset />
 
-    <!-- ComboBox -->
-    <div
-        class="flex flex-col justify-center gap-1 relative"
-        x-data="{
+        <!-- ComboBox -->
+        <div class="flex flex-col justify-center gap-1 relative" x-data="{
             selectOpen: false,
             selectedItem: '{{ $placeholder }}',
             selectedValue: '',
             chosenText: {}
         }"
+            x-on:click.outside="selectOpen = false">
+            <!-- ComboBox Trigger -->
+            <button x-on:click="selectOpen = !selectOpen" type="button" role="combobox" x-ref="selectTrigger"
+                class="flex h-10 items-center justify-between rounded-md border bg-surface px-3 py-2 text-sm placeholder:text-muted-text focus:border-ring focus:outline-none focus:ring-1 focus:ring-ring">
+                <span class="line-clamp-1" x-text="selectedItem">{{ $placeholder }}</span>
+                <svg stroke="currentColor" fill="none" viewBox="0 0 24 24" height="1em" width="1em"
+                    xmlns="http://www.w3.org/2000/svg" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="m7 15 5 5 5-5" />
+                    <path d="m7 9 5-5 5 5" />
+                </svg>
+            </button>
 
-    x-on:click.outside="selectOpen = false"
-    >
-    <!-- ComboBox Trigger -->
-    <button
-        x-on:click="selectOpen = !selectOpen"
-        type="button"
-        role="combobox"
-        x-ref="selectTrigger"
-        class="flex h-10 items-center justify-between rounded-md border bg-surface px-3 py-2 text-sm placeholder:text-muted-text focus:border-ring focus:outline-none focus:ring-1 focus:ring-ring"
-    >
-        <span class="line-clamp-1" x-text="selectedItem">{{ $placeholder }}</span>
-        <svg
-            stroke="currentColor"
-            fill="none"
-            viewBox="0 0 24 24"
-            height="1em"
-            width="1em"
-            xmlns="http://www.w3.org/2000/svg"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-        >
-            <path d="m7 15 5 5 5-5" />
-            <path d="m7 9 5-5 5 5" />
-        </svg>
-    </button>
-
-    <!-- ComboBox Content -->
-    <div
-        x-cloak
-        x-show="selectOpen"
-        x-effect="
+            <!-- ComboBox Content -->
+            <div x-cloak x-show="selectOpen"
+                x-effect="
             if (selectOpen) {
                 $nextTick(() => {
                     if ($refs.selectTrigger.getBoundingClientRect().bottom + $el.scrollHeight > window.innerHeight && $refs.selectTrigger.getBoundingClientRect().top > window.innerHeight /2) {
@@ -93,7 +71,7 @@
                 });
             }
     "
-        x-on:scroll.window="
+                x-on:scroll.window="
             $nextTick(() => {
                 if ($refs.selectTrigger.getBoundingClientRect().bottom + $el.scrollHeight > window.innerHeight && $refs.selectTrigger.getBoundingClientRect().top > window.innerHeight /2) {
                     if (!$el.classList.contains('bottom-full')) {
@@ -108,20 +86,19 @@
                 }
             })
         "
-        x-transition
-        class="absolute w-full left-0 rounded-lg border bg-surface p-1 text-sm text-main-text z-50 overflow-hidden mt-1 max-h-[50vh] overflow-y-auto"
-    >
+                x-transition
+                class="absolute w-full left-0 rounded-lg border bg-surface p-1 text-sm text-main-text z-50 overflow-hidden mt-1 max-h-[50vh] overflow-y-auto">
 
-    {{ $slot }}
-
-
-</div>
+                {{ $slot }}
 
 
+            </div>
 
-</div>
 
 
-</div>
+        </div>
+
+
+    </div>
 
 </mijnui:with-field>
