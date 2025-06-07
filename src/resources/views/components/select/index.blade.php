5eb9e4b840959c585a153b076429a95e42a271d6
Zai185 <mkhant185@gmail.com>
2025-06-07 17:46:43 +0700

    add multiple to select component

--- src/resources/views/components/select/index.blade.php
+++ src/resources/views/components/select/index.blade.php
@@ -38,6 +38,7 @@
             selectedValue: '',
             chosenText: {}
         }" @isset($name) name="{{ $name }}" @endisset
+            {{ $multiple ? 'multiple' : '' }}
             {{ $attributes->merge([
                 'class' => $class,
             ]) }}>
@@ -46,7 +47,7 @@
     @else
         <div x-data="{
             value: @if ($attributes->wire('model')) @entangle($attributes->wire('model'))
-        @else 
+            @else
             @if ($multiple) [] @else null @endif
             @endif
         }">
@@ -56,15 +57,42 @@
             <!-- ComboBox -->
             <div class="flex flex-col justify-center gap-1 relative" x-data="{
                 selectOpen: false,
-                selectedItem: value ?? '{{ $placeholder }}',
-                selectedValue: @if ($multiple) [] @else '' @endif,
+                selectedItem: @if ($multiple) value?.length ? value : [] @endif ?? '{{ $placeholder }}',
+                selectedValue: {{ $multiple ? '[]' : "''" }},
                 chosenText: {}
             }"
                 x-on:click.outside="selectOpen = false">
                 <!-- ComboBox Trigger -->
-                <button x-on:click="selectOpen = !selectOpen" type="button" role="combobox" x-ref="selectTrigger"
+                <button x-on:click.self="selectOpen = !selectOpen" type="button" role="combobox" x-ref="selectTrigger"
                     class="flex h-10 items-center justify-between rounded-md border px-3 py-2 text-sm placeholder:text-muted-text focus:border-ring focus:outline-none focus:ring-1 focus:ring-ring {{ $variantClass }} {{ $sizeClass }}">
-                    <span class="line-clamp-1" x-text="selectedItem">{{ $placeholder }}</span>
+
+                    @if ($multiple)
+                        <span class="line-clamp-1 flex gap-1 items-center">
+                            <template x-for="item in selectedItem">
+                                <mijnui:badge color="primary" size="xs">
+                                    <span x-text="item"></span>
+                                    <button type="button"
+                                        x-on:click="
+                                            const index = selectedItem.indexOf(item);
+                                            if (index > -1) {
+                                                selectedItem.splice(index, 1);
+                                                selectedValue.splice(index, 1);
+                                            }
+                                        ">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="size-4">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M6 18 18 6M6 6l12 12" />
+                                        </svg>
+                                    </button>
+                                </mijnui:badge>
+                            </template>
+                        </span>
+                    @else
+                        <span class="line-clamp-1" x-text="selectedItem">{{ $placeholder }}</span>
+                    @endif
+
+
                     <svg stroke="currentColor" fill="none" viewBox="0 0 24 24" height="1em" width="1em"
                         xmlns="http://www.w3.org/2000/svg" stroke-width="2" stroke-linecap="round"
                         stroke-linejoin="round">
@@ -76,22 +104,21 @@
                 <!-- ComboBox Content -->
                 <div x-cloak x-show="selectOpen"
                     x-effect="
-            if (selectOpen) {
-                $nextTick(() => {
-                    if ($refs.selectTrigger.getBoundingClientRect().bottom + $el.scrollHeight > window.innerHeight && $refs.selectTrigger.getBoundingClientRect().top > window.innerHeight /2) {
-                        if (!$el.classList.contains('bottom-full')) {
-                            $el.classList.add('bottom-full');
-                            $el.classList.remove('top-full');
+                    if (selectOpen) {
+                        $nextTick(() => {
+                        if ($refs.selectTrigger.getBoundingClientRect().bottom + $el.scrollHeight > window.innerHeight && $refs.selectTrigger.getBoundingClientRect().top > window.innerHeight /2) {
+                            if (!$el.classList.contains('bottom-full')) {
+                                $el.classList.add('bottom-full');
+                                $el.classList.remove('top-full');
+                            }
+                        } else {
+                            if (!$el.classList.contains('top-full')) {
+                                $el.classList.add('top-full');
+                                $el.classList.remove('bottom-full');
+                            }
                         }
-                    } else {
-                        if (!$el.classList.contains('top-full')) {
-                            $el.classList.add('top-full');
-                            $el.classList.remove('bottom-full');
-                        }
-                    }
-                });
-            }
-    "
+                    });
+                }"
                     x-on:scroll.window="
             $nextTick(() => {
                 if ($refs.selectTrigger.getBoundingClientRect().bottom + $el.scrollHeight > window.innerHeight && $refs.selectTrigger.getBoundingClientRect().top > window.innerHeight /2) {
@@ -108,7 +135,7 @@
             })
         "
                     x-transition
-                    class="absolute w-full left-0 rounded-lg border bg-surface p-1 text-sm text-main-text z-50 overflow-hidden mt-1 max-h-[50vh] overflow-y-auto ">
+                    class="absolute w-full left-0 space-y-px rounded-lg border bg-surface p-1 text-sm text-main-text z-50 overflow-hidden mt-1 max-h-[50vh] overflow-y-auto ">
 
                     {{ $slot }}
 
--- src/resources/views/components/select/option.blade.php
+++ src/resources/views/components/select/option.blade.php
@@ -4,21 +4,22 @@
     'disabled' => false,
 ])
 
-@aware(['native'])
+@aware(['native', 'multiple'])
 
 @if ($native)
-    <option value="{{$value}}" {{$selected ? 'selected' : ''}} {{$disabled ? 'disabled' : ''}}>{{$slot}}</option>
+    <option value="{{ $value }}" {{ $selected ? 'selected' : '' }} {{ $disabled ? 'disabled' : '' }}>
+        {{ $slot }}</option>
 @else
     <button
         x-on:click="
-        selectedItem = '{{ $slot }}';
-        selectedValue = '{{ $value }}';
+        {{ $multiple ? "selectedItem.push('$slot')" : "selectedItem = '$slot'" }}
+        {{ $multiple ? "selectedValue.push('$value')" : "selectedValue = '$value'" }}
         selectOpen = false;
-        value = '{{ $value }}';
+        value = selectedValue;
     "
         x-init="chosenText['{{ $value }}'] = '{{ $slot }}'"
         :class="{
-            'inline-flex w-full cursor-pointer items-center justify-between gap-2 rounded-md bg-primary/20 px-4 py-2 text-left text-sm text-primary': selectedValue === '{{ $value }}' &&
+            'inline-flex w-full cursor-pointer items-center justify-between gap-2 rounded-md bg-primary/20 px-4 py-2 text-left text-sm text-primary': (selectedValue === '{{ $value }}' || selectedValue.includes('{{$value}}')) &&
                 !{{ $disabled ? 'true' : 'false' }},
             'w-full cursor-pointer rounded-md px-4 py-2 text-left text-sm hover:bg-accent hover:text-accent-text': selectedValue !== '{{ $value }}' &&
                 !{{ $disabled ? 'true' : 'false' }},
@@ -26,7 +27,7 @@
         }"
         type="button" {{ $disabled ? 'disabled' : '' }}>
         {{ $slot }}
-        <template x-if="selectedValue === '{{ $value }}'">
+        <template x-if="selectedValue === '{{ $value }}' || selectedValue.includes('{{$value}}')">
             <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round"
                 stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                 <polyline points="20 6 9 17 4 12" />
