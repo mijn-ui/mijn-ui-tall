ea20572c8cae6af74ca9b8f6e6a61f1cb4f1b54e
Zai185 <mkhant185@gmail.com>
2025-11-13 00:00:03 +0700

    update double sidebar css

--- src/resources/views/components/sidebar/double/content.blade.php
+++ src/resources/views/components/sidebar/double/content.blade.php
@@ -7,7 +7,7 @@
     <div class="w-[14rem]" x-cloak x-show="$store.sidebar.activeContent === '{{ $value }}'">
         @if (!empty($title))
             <div class="flex items-center gap-1 px-4 pt-6">
-                <h5 class="text-xs font-semibold uppercase text-muted-text">{{ $title }}</h5>
+                <h5 class="text-xs font-semibold uppercase text-foreground">{{ $title }}</h5>
             </div>
         @endif
         {{ $slot }}
