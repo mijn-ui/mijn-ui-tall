9786009544083d91524a0cd4fde3953e5ca5710e
Zai185 <mkhant185@gmail.com>
2026-02-18 23:35:01 +0700

    fix bug for dropdown component slot error

--- src/resources/views/components/dropdown/header.blade.php
+++ src/resources/views/components/dropdown/header.blade.php
@@ -2,8 +2,10 @@
     $base = 'px-3 py-1.5 sm:px-4 sm:py-2';
 @endphp
 
-<div {{$attributes->merge([
+<x-slot:header>
+    <div {{$attributes->merge([
     'class' => $base
 ])}}>
-    {{ $slot }}
-</div>
+        {{ $slot }}
+    </div>
+</x-slot:header>
