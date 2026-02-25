8b39ed44df16b7cb8be1aa6bbe4850cddd2df53d
Zai185 <mkhant185@gmail.com>
2026-02-25 09:20:06 +0700

    feat: Introduce sidebar toggle and header navbar components, alongside a new comprehensive CSS theming system.

--- src/resources/views/components/header/navbar.blade.php
+++ src/resources/views/components/header/navbar.blade.php
@@ -1,7 +1,6 @@
 @props([
     'class' => '',
 ])
-
 @php
     $base = 'flex items-center gap-2';
 @endphp
@@ -10,23 +9,15 @@
     <div class="flex items-center gap-4">
         <button @click="$store.sidebar.toggle()"
             class="inline-flex h-8 w-8 items-center justify-center gap-1 rounded-md border border-main-border bg-transparent text-sm text-foreground transition-colors duration-200 ease-in-out hover:bg-accent hover:text-accent-text focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-main active:brightness-90 disabled:pointer-events-none disabled:opacity-50 sm:h-10 sm:w-10">
-            <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M1.66699 9C1.66699 5.92572 1.66699 4.38858 2.34517 3.29897C2.    59608 2.89585 2.90775 2.54522 3.26608 2.26295C4.23462 1.5 5.60097 1.5 8.33366 1.5H11.667C14.3997 1.5 15.766 1.5 16.7346 2.26295C17.0929 2.54522 17.4046 2.89585 17.6555 3.29897C18.3337 4.38858 18.3337 5.92572 18.3337 9C18.3337 12.0743 18.3337 13.6114 17.6555 14.701C17.4046 15.1041 17.0929 15.4548 16.7346 15.737C15.766 16.5 14.3997 16.5 11.667 16.5H8.33366C5.60097 16.5 4.23462 16.5 3.26608 15.737C2.90775 15.4548 2.59608 15.1041 2.34517 14.701C1.66699 13.6114 1.66699 12.0743 1.66699 9Z"
-                    stroke="#737373"
-                    stroke-width="1.5" />
-                <path d="M7.91699 1.5L7.91699 16.5" stroke="#737373" stroke-width="1.5" stroke-linejoin="round" />
-                <path
-                    d="M4.16699 4.83337H5.00033M4.16699 7.33337H5.00033"
-                    stroke="#737373"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round" />
+             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M2 12C2 8.31087 2 6.4663 2.81382 5.15877C3.1149 4.67502 3.48891 4.25427 3.91891 3.91554C5.08116 3 6.72077 3 10 3H14C17.2792 3 18.9188 3 20.0811 3.91554C20.5111 4.25427 20.8851 4.67502 21.1862 5.15877C22 6.4663 22 8.31087 22 12C22 15.6891 22 17.5337 21.1862 18.8412C20.8851 19.325 20.5111 19.7457 20.0811 20.0845C18.9188 21 17.2792 21 14 21H10C6.72077 21 5.08116 21 3.91891 20.0845C3.48891 19.7457 3.1149 19.325 2.81382 18.8412C2 17.5337 2 15.6891 2 12Z" />
+                <path d="M9.5 3L9.5 21" />
+                <path d="M5 7H6M5 10H6" />
             </svg>
         </button>
 
     </div>
-
+ 
     <div {{ $attributes->merge(['class' => "$base $class"]) }}>
        {{$slot}}
     </div>
