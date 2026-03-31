7fc0e95cc60f9d91ea07fce82f9f599821f8090e
Zai185 <mkhant185@gmail.com>
2026-03-31 14:07:23 +0700

    fix: Correct JavaScript interpolation in sidebar button component

--- src/resources/views/components/sidebar/button.blade.php
+++ src/resources/views/components/sidebar/button.blade.php
@@ -11,8 +11,8 @@
 
 <mijnui:button :title="$title" variant="ghost" size="icon-sm"
     x-bind:class="{
-        'bg-primary text-primary-foreground': $store.sidebar.activeContent === @js($name)
+        'bg-primary text-primary-foreground': $store.sidebar.activeContent === '{{$name}}'
     }"
-    x-on:click="$store.sidebar.setActiveContent(@js($name))">
+    x-on:click="$store.sidebar.setActiveContent('{{$name}}')">
     {{ $slot }}
 </mijnui:button>
